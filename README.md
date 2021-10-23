611555d826dcbaeaebeb9dab243160350439b571
zaid <zaalsabb@uwaterloo.ca>
2021-10-23 13:07:38 -0400

    update

--- README.md
+++ README.md
@@ -7,43 +7,71 @@ This package uses python 3, so you must create a separate catkin workspace that'
 
 1) Install these dependencies:
 
+First, let's install some tools we’ll need for the build process
 ```bash
-sudo apt-get install python3-pip python3-yaml python-catkin-tools python3-dev python3-numpy
+sudo apt-get install python3-pip python3-yaml python-catkin-tools python3-dev python3-numpy python3-catkin-pkg-modules python3-rospkg-modules
 sudo pip3 install rospkg catkin_pkg
 ```
-
-2) recursively clone this repository to your src folder in your new catkin workspace.
-
-``` bash
-mkdir ~/catkin3_ws && cd ~/catkin3_ws
-mkdir src && cd src
-git clone --recursive https://github.com/zaalsabb/yolox_ros.git
+Now, create new catkin_ws_py3 to avoid any future problems with catkin_make(assuming you are using it) and config catkin to use your python 3(3.6 in my case) when building packages:
+```bash
+mkdir ~/catkin_ws_py3 && cd ~/catkin_ws_py3 && mkdir src
+catkin config -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so
+catkin config --install
+```
+clone official vision_opencv repo:
+```bash
+cd ~/catkin_ws_py3
+cd src
+git clone -b melodic https://github.com/ros-perception/vision_opencv.git
+```
+Build tf2_ros using Python3
+```bash
+wstool init
+wstool set -y src/geometry2 --git https://github.com/ros/geometry2 -v 0.6.5
+wstool up
+rosdep install --from-paths src --ignore-src -y -r
 ```
 
-3) Install all the requirement for the [YOLOX](https://github.com/MACILLAS/YOLOX) repository.
+2) recursively clone the [YOLOX ROS wrapper repository](https://github.com/zaalsabb/yolox_ros) to your src folder in your new catkin workspace.
 
-``` bash
-cd ~/catkin3_ws/src/yolox_ros/src/YOLOX
+```bash
+cd ~/catkin_ws_py3/src
+git clone --recursive https://github.com/zaalsabb/yolox_ros.git
+```
+3) Install all the requirement for the [YOLOX](https://github.com/zaalsabb/YOLOX) repository.
+
+```bash
+cd ~/catkin_ws_py3/src/yolox_ros/src/YOLOX
 pip3 install -r requirements.txt
 ```
 
 4) Configure and build the catkin workspace using catkin_make.
 
-``` bash
-catkin config -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so
+```bash
+cd ~/catkin_ws_py3
+catkin_make --cmake-args \
+            -DCMAKE_BUILD_TYPE=Release \
+            -DPYTHON_EXECUTABLE=/usr/bin/python3 \
+            -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m \
+            -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so
 catkin config --install
 catkin_make
 ```
-
 Note: you need to run the setup file each time you open a new terminal, since yolox_ros cannot be started from a launch file in other catkin workspaces.
-``` bash
-cd ~/catkin3_ws
+```bash
+cd ~/catkin_ws_py3
 source devel/setup.sh
 ```
-
-## Usage
-
+Edit the ```~/.bashrc``` file and add the following lines (replace melodic with kinetic if you are on Ubuntu 16):
 ```bash
+source /opt/ros/melodic/setup.bash
+source ~/catkin_ws_py3/devel/setup.bash
+```
+
+#### Usage
+Open a new terminal, then enter the following:
+```bash
+source ~/catkin_ws_py3/devel/setup.sh
 roslaunch yolox_ros yolox.launch
 ```
 
--- src/yolox.py
+++ src/yolox.py
@@ -15,29 +15,34 @@ sys.path.insert(0,YOLOX_module)
 
 from YOLOX import detspall
 
-from yolox_ros.msg import BoundingBox, BoundingBoxes2D
+from yolox_ros.msg import BoundingBox, SegmentImage
 
 class Node:
 
     def __init__(self):
 
         rospy.init_node('yolox')
+        self.tfBuffer = tf2_ros.Buffer()        
+        ls = tf2_ros.TransformListener(self.tfBuffer)
+        
         self.model = rospy.get_param('~model', default=os.path.join(YOLOX_module,'yolox.onnx'))       
         self.segment_rate = rospy.get_param('~segment_rate', default=1) 
         self.num_images = rospy.get_param('~num_images', default=8)         
+        self.map_frame_id = rospy.get_param('~map_frame_id', default='map')       
 
         sub1=message_filters.Subscriber('/image', Image)
         sub2=message_filters.Subscriber('/camera_info', CameraInfo)
 
         self.pub1 = rospy.Publisher('/bounding_box',BoundingBox,queue_size=1)
         self.pub2 = rospy.Publisher('/debug/image',Image,queue_size=1)
+        self.pub3 = rospy.Publisher('/segment_image',SegmentImage,queue_size=1)
 
         ts = message_filters.ApproximateTimeSynchronizer([sub1, sub2], 1, 0.5) 
         ts.registerCallback(self.callback)
 
         self.spallDetector = detspall(model=self.model)
         # cache dataframe
-        self.bb_df = pd.DataFrame(columns=['file', 'x1', 'y1', 'x2', 'y2', 'track'])
+        # self.bb_df = pd.DataFrame(columns=['file', 'x1', 'y1', 'x2', 'y2', 'track'])
         self.lastTrackIdx = 0
         self.i = 0
         self.t1 = rospy.get_time()
@@ -51,19 +56,22 @@ class Node:
         img = imgmsg_to_cv2(args[0])
         caminfo = args[1]
         bbsTrack,conf = self.spallDetector.detAndTrack(img)
+        
 
         if bbsTrack.size > 0 and conf > self.current_object_thres: #check if returned a bounding box            
             bbsTrack = bbsTrack[0]
+            bb_msg = self.sendBoundingBoxMsg(bbsTrack,conf,caminfo)
             #print(bbsTrack)
             if bbsTrack[4] == self.lastTrackIdx: #still tracking same object
                 self.current_object_thres = 0
-                entry = pd.DataFrame(
-                        {'file': '', 'x1': bbsTrack[0], 'y1': bbsTrack[1], 'x2': bbsTrack[2], 'y2': bbsTrack[3], 'track': bbsTrack[4]}, index=[self.i])
-                self.bb_df = self.bb_df.append(entry)
+                # entry = pd.DataFrame(
+                #         {'file': '', 'x1': bbsTrack[0], 'y1': bbsTrack[1], 'x2': bbsTrack[2], 'y2': bbsTrack[3], 'track': bbsTrack[4]}, index=[self.i])
+                # self.bb_df = self.bb_df.append(entry)
                 self.i += 1
                 self.t2 = rospy.get_time()
                 if (self.t2-self.t1) > 1/self.segment_rate:
-                    pass
+                    self.sendSegmentImg(img,bb_msg,self.i,caminfo.header.frame_id)
+                    self.t1 = self.t2
                 
                 # real implementation we will need to save the images in a different directory
             else: #starting to track the next object (TRIGGER)
@@ -71,28 +79,43 @@ class Node:
                 self.lastTrackIdx = bbsTrack[4]
                 # need to sample the dataframe for number of images
                 #bb_df_elements = self.bb_df.sample(n=self.num_images) #randomly sample 8 elements
-            self.sendMsg(bbsTrack,conf,caminfo)
-            self.sendDebugImg(img,bbsTrack)
+            
+            self.sendDebugImg(img,bbsTrack)     
 
-    def sendSegmentImg(self,img,bbsTrack,i):
 
-        img_msg = cv2_to_imgmsg(img)
-        self.pub2.publish(img_msg) 
+    def sendSegmentImg(self,img,bb_msg,i,camera_frame_id):
 
-    def sendMsg(self,bbsTrack,conf,caminfo):
+        msg = SegmentImage()
+        try:
+            trans = self.tfBuffer.lookup_transform(self.map_frame_id, camera_frame_id, bb_msg.header.stamp)
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
+            return 
+
+        msg.header.stamp = bb_msg.header.stamp
+        msg.header.frame_id = camera_frame_id
+        msg.image = cv2_to_imgmsg(img)
+        msg.bbox = bb_msg
+        msg.transform = trans.transform
+        self.pub3.publish(msg) 
+
+    def bb_to_msg(self,bbsTrack,conf,caminfo):
         bb = BoundingBox()
         bb.header.stamp = caminfo.header.stamp
-        bb.header.frame_id = "detection"
-        bb.camera_info = caminfo        
-        bb.xmin = int(bbsTrack[0])
-        bb.ymin = int(bbsTrack[1])
-        bb.xmax = int(bbsTrack[2])
-        bb.ymax = int(bbsTrack[3])
-        bb.id = bbsTrack[4]
-        bb.probability = conf
-        bb.Class = "spalling"        
-
+        bb.header.frame_id = self.map_frame_id
+        bb.camera_info = caminfo
+        bb.x1 = int(bbsTrack[0])
+        bb.y1 = int(bbsTrack[1])
+        bb.x2 = int(bbsTrack[2])
+        bb.y2 = int(bbsTrack[3])
+        bb.confidence = conf
+        bb.track = int(bbsTrack[4])
+        bb.Class = "spalling"
+        return bb        
+ 
+    def sendBoundingBoxMsg(self,bbsTrack,conf,caminfo):
+        bb=self.bb_to_msg(bbsTrack,conf,caminfo)
         self.pub1.publish(bb)
+        return bb
 
     def sendDebugImg(self,img,bbsTrack):
         if bbsTrack[4] % 2 == 0:
@@ -103,71 +126,6 @@ class Node:
         msg = cv2_to_imgmsg(img)
         self.pub2.publish(msg)      
 
-    #     final_boxes, final_scores, final_cls_inds = self.run(sess=self.session, img=image, visual=False)
-    #     msg_bb = BoundingBoxes2D()
-    #     msg_bb.header.stamp = rospy.Time()
-    #     msg_bb.header.frame_id = "detection"
-    #     msg_bb.camera_info = caminfo_msg
-    #     if final_boxes is not None:
-    #         for box,score in zip(final_boxes,final_scores):
-    #             bb = BoundingBox()
-    #             bb.Class = "spalling"
-    #             bb.xmin = int(box[0])
-    #             bb.ymin = int(box[1])
-    #             bb.xmax = int(box[2])
-    #             bb.ymax = int(box[3])
-    #             bb.probability = score
-    #             msg_bb.bounding_boxes.append(bb)
-
-    #             if score > 0.5:
-    #                 image = cv2.rectangle(image, (int(bb.xmin),int(bb.ymin)), (int(bb.xmax),int(bb.ymax)), (255, 0, 0), 3)
-    #             else:
-    #                 image = image                
-        
-    #     self.pub1.publish(msg_bb)
-    #     msg = cv2_to_imgmsg(image)
-    #     self.pub2.publish(msg)       
-        
-    # def open_sess(self,model='yolox.onnx'):
-    #     return onnxruntime.InferenceSession(model)
-
-    # def run(self,sess=None, img=None, input_shape="416,416", score=0.3, visual=False):
-    #     input_shape = tuple(map(int, input_shape.split(',')))
-    #     origin_img = img
-    #     mean = (0.485, 0.456, 0.406)
-    #     std = (0.229, 0.224, 0.225)
-    #     img, ratio = preprocess(origin_img, input_shape, mean, std)
-    #     session = sess
-
-    #     ort_inputs = {session.get_inputs()[0].name: img[None, :, :, :]}
-    #     output = session.run(None, ort_inputs)
-    #     predictions = demo_postprocess(output[0], input_shape)[0]
-
-    #     boxes = predictions[:, :4]
-    #     scores = predictions[:, 4:5] * predictions[:, 5:]
-
-    #     boxes_xyxy = np.ones_like(boxes)
-    #     boxes_xyxy[:, 0] = boxes[:, 0] - boxes[:, 2] / 2.
-    #     boxes_xyxy[:, 1] = boxes[:, 1] - boxes[:, 3] / 2.
-    #     boxes_xyxy[:, 2] = boxes[:, 0] + boxes[:, 2] / 2.
-    #     boxes_xyxy[:, 3] = boxes[:, 1] + boxes[:, 3] / 2.
-    #     boxes_xyxy /= ratio
-    #     dets = multiclass_nms(boxes_xyxy, scores, nms_thr=0.45, score_thr=0.1)
-
-    #     if dets is not None:
-    #         final_boxes, final_scores, final_cls_inds = dets[:, :4], dets[:, 4], dets[:, 5]
-    #     else:
-    #         return None, None, None
-
-    #     if visual:
-    #         COCO_CLASSES = '0'
-    #         origin_img = vis(origin_img, final_boxes, final_scores, final_cls_inds,
-    #                         conf=score, class_names=COCO_CLASSES)
-    #         cv2.imwrite('output.png', origin_img)
-
-    #     return final_boxes, final_scores, final_cls_inds
-
-      
 
 if __name__ == '__main__':
     Node() 
